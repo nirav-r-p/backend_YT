1e40b8ff0a81513c80d2aec9af3aafc925bcb625
nirav-r-p <prajapatinirav243@gmail.com>
2024-02-10 16:37:51 +0530

    add utils file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.on('error',(e)=>{
+       console.log("Error:",e);
+    })
+    app.listen(process.env.Port || 8000,()=>{
+        console.log(`Server running at port :${process.env.Port}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection falid !!",err);
+})
